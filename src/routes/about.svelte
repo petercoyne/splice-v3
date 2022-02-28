8a2385667efdd63ee81af1ba0bbc02dc651d5874
Peter Coyne <61690004+petercoyne@users.noreply.github.com>
2022-02-28 03:03:21 +0000

    🎵 i think they align now...

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -20,7 +20,7 @@
 			<img src="/profile.webp" alt="Profile" class="self-start lg:mt-8" />
 			<div class="vertical-divider border-r border-neutral-700 h-full sm:hidden"></div>
 			<div>
-				<h3 class="text-3xl text-white mb-2">Hello :)</h3>
+				<h3 class="text-3xl text-white mb-2 lg:mt-2">Hello :)</h3>
 				<p class="leading-6 text-sm sm:text-base">
 					I'm a designer &amp; frontend developer based in the west of Ireland. This site is a platform to
 					play with ideas for now, my plan is to get a bit more experience with development before I start
@@ -32,13 +32,13 @@
 	<div>
 		<h4 class="uppercase">Tools of the trade</h4>
 		<hr class="my-4"/>
-		<div class="grid grid-cols-1 sm:grid-cols-[8rem_auto] lg:grid-cols-[10rem_auto] gap-4 md:gap-8 items-center">
+		<div class="grid grid-cols-1 sm:grid-cols-[8rem_auto] lg:grid-cols-[10rem_auto] gap-4 md:gap-8 items-start">
 			<div class="self-start lg:mt-8 flex items-center justify-center max-w-full">
 				<img src="/sveltekit-logo-noglow.webp" alt="SvelteKit" class="shrink min-w-0 max-h-32 m-4 sm:scale-125"/>
 				<img src="/tailwindlogo.png" alt="TailwindCSS" class="shrink min-w-0 max-h-32 m-4 bg-neutral-300 rounded-lg sm:hidden"/>
 			</div>
 			<div>
-				<h3 class="text-3xl text-white mb-2 text-center sm:text-left hidden lg:block">Technologies</h3>
+				<h3 class="text-3xl text-white my-2 text-center sm:text-left hidden lg:block">Technologies</h3>
 				<p class="leading-6 text-sm sm:text-base text-center sm:text-left">
 					I develop sites using modern frontend development tools such as <a href="http://kit.svelte.dev">SvelteKit</a>,
 					<a href="http://tailwindcss.com">TailwindCSS</a>, <a href="http://github.com">GitHub</a> and
