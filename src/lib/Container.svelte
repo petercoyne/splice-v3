db69d1612237df9b40d7f67c0aa365d0e54df5e1
Peter Coyne <61690004+petercoyne@users.noreply.github.com>
2022-03-01 01:25:03 +0000

    bit of padding on header

--- src/lib/Container.svelte
+++ src/lib/Container.svelte
@@ -1,3 +1,3 @@
-<div class="max-w-screen-2xl mx-auto p-6 sm:p-10 md:p-12 mt-20 sm:mt-24 xl:mt-28 xl:p-24 !pb-0">
+<div class="max-w-screen-2xl mx-auto p-6 sm:p-10 md:p-12 mt-20 sm:mt-24 xl:mt-32 xl:p-24 !pb-0">
 	<slot/>
 </div>
